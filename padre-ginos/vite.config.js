e38cbacdd6ad87adaa91fb70ef03818874d8cfc2
Mykola Kuievda <dankraig23@gmail.com>
2025-03-28 12:31:15 +0000

    Modify vite.config.js to add server endpoints

--- padre-ginos/vite.config.js
+++ padre-ginos/vite.config.js
@@ -2,19 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
-// replace export
 export default defineConfig({
   server: {
     fs: {
-      allow: [".."], 
+      allow: [".."],
     },
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: process.env.VITE_API_URL || "http://localhost:3000", // Use environment variable or fallback to localhost
         changeOrigin: true,
       },
       "/public": {
-        target: "http://localhost:3000",
+        target: process.env.VITE_API_URL || "http://localhost:3000", // Use environment variable or fallback to localhost
         changeOrigin: true,
       },
     },
