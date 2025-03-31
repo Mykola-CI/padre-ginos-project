3cff1981eb7eb1267c896601773011d225ba552c
Mykola Kuievda <dankraig23@gmail.com>
2025-03-31 21:10:02 +0100

    Fix error in import tanstack router plugin

--- padre-ginos/vite.config.js
+++ padre-ginos/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { TanStackRouterVite } from '@tanstack/router-plugin';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 export default defineConfig({
   plugins: [
     react(),
     TanStackRouterVite({
+      target: 'react', // Specify React as the target framework
+      autoCodeSplitting: true, // Enable automatic code splitting for routes
       routesDirectory: './src/routes', // Directory where your route files are located
       generatedRouteTree: './src/routeTree.gen.ts', // Path where the route tree will be generated
     }),
