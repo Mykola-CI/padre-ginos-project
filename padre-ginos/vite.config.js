2bbec342015d5430b7cbb408bf5d149db7c09b81
Mykola Kuievda <dankraig23@gmail.com>
2025-03-31 21:01:56 +0100

    Add plugin to vite.config to dynamically generate routeTree.gen.ts

--- padre-ginos/vite.config.js
+++ padre-ginos/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { TanStackRouterVite } from '@tanstack/router-plugin';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    TanStackRouterVite({
+      routesDirectory: './src/routes', // Directory where your route files are located
+      generatedRouteTree: './src/routeTree.gen.ts', // Path where the route tree will be generated
+    }),
+  ],
   server: {
     port: 10000, // Frontend development server runs on port 10000
     proxy: {
