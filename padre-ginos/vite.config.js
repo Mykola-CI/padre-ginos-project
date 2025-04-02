36541a947b0b5fee3378971c2d874567c9ee110b
Mykola Kuievda <dankraig23@gmail.com>
2025-04-02 14:04:48 +0100

    Reinstate tanstackrouter vite plugin

--- padre-ginos/vite.config.js
+++ padre-ginos/vite.config.js
@@ -2,9 +2,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    TanStackRouterVite({
+      target: 'react',
+      autoCodeSplitting: true,
+      routesDirectory: './src/routes', // Explicitly define routes directory
+      generatedRouteTree: './src/routeTree.gen.ts', // Explicit output path
+    }),
+    react()
+  ],
   base: '/padre-ginos-react/',
   publicDir: 'public',
   build: {
