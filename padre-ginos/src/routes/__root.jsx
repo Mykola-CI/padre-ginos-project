d3eab293d9140687feb3ced87d58e2933ad14d87
Mykola Kuievda <dankraig23@gmail.com>
2025-04-04 23:29:33 +0100

    Add children default route for outlet element in __root.jsx

--- padre-ginos/src/routes/__root.jsx
+++ padre-ginos/src/routes/__root.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, createRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import PizzaOfTheDay from "../PizzaOfTheDay";
 import Header from "../Header";
 import { CartContext } from "../contexts";
+import Index from "./index.lazy";
 
 export const Route = createRootRoute({
   component: () => {
@@ -23,4 +24,10 @@ export const Route = createRootRoute({
       </>
     );
   },
+  children: [
+    createRoute({
+      path: "/", // Default index route
+      component: Index,
+    }),
+  ],
 });
